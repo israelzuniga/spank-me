43898ea4af42a0e95703e55a220524e17f17e15d
Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>
2018-09-14 18:41:15 -0500

    MapReduce
    Signed-off-by: Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>

--- scala/wordcount/src/main/scala/WordCount.scala
+++ scala/wordcount/src/main/scala/WordCount.scala
@@ -1,3 +1,26 @@
-object WordCount {
+import org.apache.spark.{SparkConf, SparkContext}
 
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //Create a SparkContext to initialize Spark
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("Word Count")
+    val sc = new SparkContext(conf)
+
+    /*
+    Load the text file into a Spark RDD.
+    Note:
+    If we are running locally, we must use the full local path of our file.
+    If we are running in a spark cluster, we must format the file's path depending on our filesystem (HDFS, S3, etc)
+     */
+    val textFile = sc.textFile("quijote.txt")
+
+    // Perform the word count
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+
+
+  }
 }
