a59369df5ec65c302f8fa26894f8cf76acf0a97d
Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>
2018-09-14 17:55:12 -0500

    Created SparkContext

--- scala/hello-world/src/main/scala/HelloWorld.scala
+++ scala/hello-world/src/main/scala/HelloWorld.scala
@@ -1,3 +1,14 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
 object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    // Create SparkContext to initialize Spark
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("Hello World")
+    val sc = new SparkContext(conf)
+
+
+  }
 
 }
